d928d0cbb2f99b6b96315be304fe5dcccdc58837
BRITVA-hp <borbritvahp@gmail.com>
2022-10-06 14:17:15 +0400

    start reviews

--- js/script.js
+++ js/script.js
@@ -244,4 +244,21 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 
+    //faq
+    const tabs = document.querySelectorAll('.faq__item')
+    const tabsTriggers = document.querySelectorAll('.faq__item__header')
+    const tabsContents = document.querySelectorAll('.faq__item__text')
+
+    tabsTriggers.forEach((trigger, triggerIndex) => {
+        trigger.addEventListener('click', () => {
+            tabs[triggerIndex].classList.toggle('faq__item--active')
+            if (tabs[triggerIndex].classList.contains('faq__item--active')) {
+                tabsContents[triggerIndex].style.height= tabsContents[triggerIndex].scrollHeight + 'px'
+            } else {
+                tabsContents[triggerIndex].style = ''
+            }
+        })
+    })
+    
+
 })
