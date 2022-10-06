9545a09182cac3a195cbb4d646ca225a2aac7d1a
BRITVA-hp <borbritvahp@gmail.com>
2022-10-06 19:07:28 +0400

    start terms

--- js/script.js
+++ js/script.js
@@ -259,6 +259,39 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
     })
+
+    //video
+    function video(triggers, _video, _modalVideo, _modalVideoClose) {
+        const play = document.querySelectorAll(triggers),
+            video = document.querySelector(_video),
+            modalVideo = document.querySelector(_modalVideo),
+            modalVideoClose = document.querySelector(_modalVideoClose);
+
+        play.forEach(item => {
+            item.addEventListener('click', (e) => {
+                const path = e.currentTarget.getAttribute('data-video');
+                modalVideo.classList.add('modal--active');
+                document.body.style.overflow = 'hidden';
+                video.setAttribute('src', path);
+                video.play();
+            });
+        });
+
+        modalVideo.addEventListener('click', (e) => {
+            if (e.target.classList.contains('modal__container')) {
+                modalVideo.classList.remove('modal--active');
+                video.pause();
+                document.body.style.overflow = '';
+            }
+        });
+        modalVideoClose.addEventListener('click', () => {
+            modalVideo.classList.remove('modal--active');
+            video.pause();
+            document.body.style.overflow = '';
+        })
+    }
+
+    video('.reviews__play', '#video', '.modal-video', '.modal-video__close');
     
 
 })
