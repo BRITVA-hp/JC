26f5d2eface3ca3b81f3a41c7e6d658eb857cb19
BRITVA-hp <borbritvahp@gmail.com>
2022-10-07 13:55:40 +0400

    ready. ready?

--- js/script.js
+++ js/script.js
@@ -21,6 +21,28 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // burger
+    const burger = document.querySelector('.header__burger')
+    const menu = document.querySelector('.menu')
+    const menuClose = document.querySelectorAll('[data-menuClose]')
+
+    burger.addEventListener('click', () => {
+        menu.classList.add('menu--active')
+        document.body.style.overflow = 'hidden';
+    })
+    menuClose.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('menu--active')
+            document.body.style.overflow = '';
+        })
+    })
+    menu.addEventListener('click', (e) => {
+        if (e.target.classList.contains('menu')) {
+            menu.classList.remove('menu--active')
+            document.body.style.overflow = '';
+        }
+    })
+
     // range, calc
 
     // маска
@@ -292,6 +314,76 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     video('.reviews__play', '#video', '.modal-video', '.modal-video__close');
+
+    // text hide
+    const button = document.querySelector('.terms__link'),
+          text = document.querySelector('.terms__hide');
+
+    let textHide = false;
+
+    button.addEventListener('click', () => {
+        if(!textHide) {
+            text.style.height = text.scrollHeight + 'px'
+            button.innerHTML = 'Скрыть'
+            textHide = true;
+        } else {
+            text.style = ''
+            button.innerHTML = 'Читать далее <span>&#8250;</span>'
+            textHide = false;
+        }
+    })
+
+
+    // функция для модалки
+
+    function calcScroll() {
+        let div = document.createElement('div');
+        
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+        
+        document.body.appendChild(div);
+        let scarollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        
+        return scarollWidth;
+    }
+
+    let scrollWidth = calcScroll();
+
+    function modal(modal, modalActiveClass, triggers, modalClose) {
+        const triggers_ = document.querySelectorAll(triggers),
+                modal_ = document.querySelector(modal),
+                modalClose_ = document.querySelector(modalClose);
+
+        if (triggers_.length > 0) {
+            triggers_.forEach(item => {
+                item.addEventListener('click', () => {
+                    modal_.classList.add(modalActiveClass);
+                    document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = `${scrollWidth}px`;
+                });
+            });
+
+            modalClose_.addEventListener('click', () => {
+                modal_.classList.remove(modalActiveClass);
+                document.body.style.overflow = '';
+                document.body.style.marginRight = '0px';
+            });
+
+            modal_.addEventListener('click', (e) => {
+                if (e.target.classList.contains('modal__container')) {
+                    modal_.classList.remove(modalActiveClass);
+                    document.body.style.overflow = '';
+                    document.body.style.marginRight = '0px';
+                }
+            });
+        }
+    }
+
+    modal('.modal-main', 'modal--active', '[data-modal]', '.modal-main__close');
     
 
 })
